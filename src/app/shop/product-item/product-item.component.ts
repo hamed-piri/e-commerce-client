1f5c79c909acf0a3e1908b0c0688a4aba32b33d1
hamed <hamedp976@gmail.com>
2023-03-26 10:00:45 +0430

    092 Passing down data to child components

--- src/app/shop/product-item/product-item.component.ts
+++ src/app/shop/product-item/product-item.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Product} from "../../shared/models/product";
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.css']
 })
-export class ProductItemComponent {
+export class ProductItemComponent implements OnInit{
+  @Input() product?: Product;
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
 
 }
