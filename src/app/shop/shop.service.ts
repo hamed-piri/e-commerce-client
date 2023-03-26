8458e028c72475815b45a8ae54792181973e09f4
hamed <hamedp976@gmail.com>
2023-03-26 08:15:23 +0430

    088 Intro to Angular services

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Pagination} from "../shared/models/pagination";
+import {Product} from "../shared/models/product";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+  baseUrl = 'https://localhost:8080/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts(){
+    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products');
+  }
 }
