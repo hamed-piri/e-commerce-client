c9b171efff39ed8495f2e39d7cfb3d2d0c59f333
hamed <hamedp976@gmail.com>
2023-03-26 20:30:00 +0430

    108 Creating the routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
 import {CoreModule} from "./core/core.module";
 import {ShopModule} from "./shop/shop.module";
+import {HomeModule} from "./home/home.module";
+import {RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -13,10 +16,13 @@ import {ShopModule} from "./shop/shop.module";
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule
+    ShopModule,
+    HomeModule,
+    RouterOutlet
   ],
   providers: [],
   bootstrap: [AppComponent]
