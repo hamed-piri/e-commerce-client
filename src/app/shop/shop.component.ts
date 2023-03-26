ab9e2a30ca6ee08bc9350b3071c6b3526ef6a860
hamed <hamedp976@gmail.com>
2023-03-26 10:26:28 +0430

    093 Adding the product filters

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Product} from "../shared/models/product";
 import {ShopService} from "./shop.service";
+import {IBrand} from "../shared/models/brand";
+import {IType} from "../shared/models/productType";
 
 @Component({
   selector: 'app-shop',
@@ -9,11 +11,41 @@ import {ShopService} from "./shop.service";
 })
 export class ShopComponent implements OnInit{
   products: Product[] = []
+  brands: IBrand[] = []
+  types: IType[] = []
   constructor(private shopService: ShopService) {
   }
   ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts(){
     this.shopService.getProducts().subscribe({
       next: response => this.products = response.data, // what to do next
+      error: error => console.log(error), // what to do if there is an error
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statement');
+      }
+    })
+  }
+
+  getBrands(){
+    this.shopService.getBrands().subscribe({
+      next: response => this.brands = response, // what to do next
+      error: error => console.log(error), // what to do if there is an error
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statement');
+      }
+    })
+  }
+
+  getTypes(){
+    this.shopService.getTypes().subscribe({
+      next: response => this.types = response, // what to do next
       error: error => console.log(error), // what to do if there is an error
       complete: () => {
         console.log('request completed');
--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Pagination} from "../shared/models/pagination";
 import {Product} from "../shared/models/product";
+import {IBrand} from "../shared/models/brand";
+import {IType} from "../shared/models/productType";
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +15,13 @@ export class ShopService {
 
   getProducts(){
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products?pageSize=50');
+  }
+
+  getBrands(){
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes(){
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
   }
 }
