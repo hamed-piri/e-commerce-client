59758b251de8b6204973d0c76f900917b4bb7764
hamed <hamedp976@gmail.com>
2023-03-26 21:39:21 +0430

    110 Making the links active

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NavBarComponent} from "./nav-bar/nav-bar.component";
-import {RouterLink} from "@angular/router";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 
 
@@ -9,10 +9,11 @@ import {RouterLink} from "@angular/router";
   declarations: [
     NavBarComponent
   ],
-    imports: [
-        CommonModule,
-        RouterLink
-    ],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive
+  ],
   exports: [
     NavBarComponent
   ]
