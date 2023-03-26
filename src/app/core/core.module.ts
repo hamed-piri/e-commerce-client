698c68f06b9f065e8afa4b24641171243e3234c4
hamed <hamedp976@gmail.com>
2023-03-26 20:44:13 +0430

    109 Setting up the nav links

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {RouterLink} from "@angular/router";
 
 
 
@@ -8,9 +9,10 @@ import {NavBarComponent} from "./nav-bar/nav-bar.component";
   declarations: [
     NavBarComponent
   ],
-  imports: [
-    CommonModule
-  ],
+    imports: [
+        CommonModule,
+        RouterLink
+    ],
   exports: [
     NavBarComponent
   ]
