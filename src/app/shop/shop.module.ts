29cbce316cba9b8138b82d0de50403259f9d83f9
hamed <hamedp976@gmail.com>
2023-03-27 06:56:00 +0430

    113 Lazy loading angular modules

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -5,6 +5,7 @@ import { ProductItemComponent } from './product-item/product-item.component';
 import {SharedModule} from "../shared/shared.module";
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import {RouterLink} from "@angular/router";
+import {ShopRoutingModule} from "./shop-routing.module";
 
 
 
@@ -21,7 +22,8 @@ import {RouterLink} from "@angular/router";
   imports: [
     CommonModule,
     SharedModule,
-    RouterLink
+    RouterLink,
+    ShopRoutingModule
   ]
 })
 export class ShopModule { }
