46a25f56cbf5993b33d08151fead1607fef144a8
hamed <hamedp976@gmail.com>
2023-03-28 11:46:35 +0430

    150 Adding the add item to basket in the component method

--- src/app/shop/product-item/product-item.component.ts
+++ src/app/shop/product-item/product-item.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Product} from "../../shared/models/product";
+import {BasketService} from "../../basket/basket.service";
 
 @Component({
   selector: 'app-product-item',
@@ -8,9 +9,13 @@ import {Product} from "../../shared/models/product";
 })
 export class ProductItemComponent implements OnInit{
   @Input() product?: Product;
-  constructor() {
+  constructor(private basketService: BasketService) {
   }
   ngOnInit(): void {
+  }
+
+  addItemToBasket(){
+    this.product && this.basketService.addItemToBasket(this.product);
   }
 
 }
